f5a5c3442a7f30931fb5e352ad7102ce7d83f2ff
hsldymq <hsldymq@qq.com>
2019-09-11 19:53:25 +0800

    refactor: time unit

--- src/Timer/Timer.php
+++ src/Timer/Timer.php
@@ -11,7 +11,15 @@ class Timer
 {
     const TICKS_PER_SEC = 1;
 
-    private $currentTick = 0;
+    /**
+     * @var int
+     */
+    private $currentTick;
+
+    /**
+     * @var int
+     */
+    private $tickDuration;
 
     /**
      * @var array [
@@ -46,34 +54,54 @@ class Timer
      */
     private $callback;
 
-    public function __construct(LoopInterface $eventLoop, callable $callback)
+    public function __construct(int $tickDuration, LoopInterface $eventLoop, callable $callback)
     {
+        $this->tickDuration = $tickDuration;
         $this->eventLoop = $eventLoop;
         $this->callback = $callback;
     }
 
     public function __destruct()
     {
-        $this->stopTimer();
+        $this->stop();
         $this->clearJobs();
     }
 
-    public function startTimer()
+    public function start()
     {
         if ($this->timer) {
             return;
         }
 
-        $this->timer = $this->eventLoop->addPeriodicTimer(1 / self::TICKS_PER_SEC, [$this, 'tick']);
+        $this->timer = $this->eventLoop->addPeriodicTimer(1 / $this->getTicksPerSec(), [$this, 'tick']);
         foreach ($this->jobInfo as $jobID => $info) {
             $this->setNextTimingTick($jobID);
         }
     }
 
-    public function stopTimer()
+    public function stop()
     {
-        $this->eventLoop->cancelTimer($this->timer);
+        if ($this->timer) {
+            $this->eventLoop->cancelTimer($this->timer);
+        }
         $this->timer = null;
+        $this->currentTick = 0;
+        $this->tickJobs = [];
+    }
+
+    public function tick()
+    {
+        ++$this->currentTick;
+
+        foreach ($this->tickJobs[$this->currentTick] ?? [] as $index => $jobID) {
+            try {
+                call_user_func($this->callback, $jobID);
+            } finally {
+                $this->setNextTimingTick($jobID);
+            }
+        }
+
+        unset($this->tickJobs[$this->currentTick]);
     }
 
     public function addJob(string $jobID, TimingInterface $timing)
@@ -102,40 +130,45 @@ class Timer
         $this->jobInfo = [];
     }
 
-    private function tick()
+    /**
+     * 返回每秒的tick数.
+     *
+     * @return float
+     */
+    public function getTicksPerSec(): float
     {
-        ++$this->currentTick;
+        return floatval(Duration::SECOND / $this->tickDuration);
+    }
 
-        foreach ($this->tickJobs[$this->currentTick] ?? [] as $index => $jobID) {
-            try {
-                call_user_func($this->callback, $jobID);
-            } finally {
-                if (isset($this->jobInfo[$jobID])) {
-                    unset($this->tickJobs[$this->currentTick][$index]);
-                    $this->setNextTimingTick($jobID);
-                }
-            }
-        }
-
-        unset($this->tickJobs[$this->currentTick]);
+    /**
+     * 返回一次tick的时间间隔.
+     *
+     * 参考Duration类.
+     *
+     * @return int
+     */
+    public function getTickDuration(): int
+    {
+        return $this->tickDuration;
     }
 
     private function setNextTimingTick($jobID)
     {
-        if (!isset($jobID)) {
+        if (!isset($this->jobInfo[$jobID])) {
             return;
         }
 
         /** @var TimingInterface $timing */
         $timing = $this->jobInfo[$jobID]['timing'];
-        $tick = $timing->getTimingTick($this->getCurrentDateTime());
+        $tick = $timing->getTimingTick($this->getCurrentDateTime(), $this);
         if ($tick <= 0) {
             return;
         }
 
-        $this->tickJobs[$tick][] = $jobID;
+        $nextTick = $this->currentTick + $tick;
+        $this->tickJobs[$nextTick][] = $jobID;
         $this->jobInfo[$jobID]['tick'] = $tick;
-        $this->jobInfo[$jobID]['indexInList'] = count($this->tickJobs[$tick]) - 1;
+        $this->jobInfo[$jobID]['indexInList'] = count($this->tickJobs[$nextTick]) - 1;
     }
 
     protected function getCurrentDateTime(): \DateTime
