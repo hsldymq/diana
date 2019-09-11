27dbe100219ce23761f1fbe733c434e38bf12d8f
hsldymq <hsldymq@qq.com>
2019-09-11 19:55:42 +0800

    add trait

---
+++ src/TailingEventEmitterTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archman\Diana;
+
+trait TailingEventEmitterTrait
+{
+    public function emit($event, array $arguments = [])
+    {
+        // 非Whisper预定义事件就加上当前对象在参数末尾
+        if (strpos($event, '__') !== 0) {
+            $arguments[] = $this;
+        }
+
+        parent::emit($event, $arguments);
+    }
+
+    public function hasEventListened($event)
+    {
+        return isset($this->onceListeners[$event]) || isset($this->listeners[$event]);
+    }
+}
