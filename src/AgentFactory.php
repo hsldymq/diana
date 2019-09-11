71bdb69186d522b5a3775010c6af7ce87fca8403
hsldymq <hsldymq@qq.com>
2019-09-11 14:05:18 +0800

    add method

--- src/AgentFactory.php
+++ src/AgentFactory.php
@@ -9,8 +9,37 @@ use Archman\Whisper\Interfaces\WorkerFactoryInterface;
 
 class AgentFactory implements WorkerfactoryInterface
 {
+    /**
+     * @var array [
+     *      [$event, $handler],
+     *      ...
+     * ]
+     */
+    private $eventHandlers = [];
+
     public function makeWorker(string $id, $socketFD): AbstractWorker
     {
-        return new Agent($id, $socketFD);
+        $agent = new Agent($id, $socketFD);
+
+        foreach ($this->eventHandlers as $each) {
+            $agent->on($each[0], $each[1]);
+        }
+
+        return $agent;
+    }
+
+    /**
+     * 注册agent的事件
+     *
+     * @param string $event
+     * @param callable $handler
+     *
+     * @return self
+     */
+    public function registerEvent(string $event, callable $handler): self
+    {
+        $this->eventHandlers[] = [$event, $handler];
+
+        return $this;
     }
 }
