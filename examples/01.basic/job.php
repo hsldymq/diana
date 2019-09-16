c7802495b5b160407d7ff3fc8a9ab331231ecad1
hsldymq <hsldymq@qq.com>
2019-09-17 01:47:25 +0800

    update example

--- examples/01.basic/job.php
+++ examples/01.basic/job.php
@@ -8,11 +8,11 @@ use Archman\Diana\Diana;
 use Archman\Diana\Timer\PeriodicTiming;
 
 $master = (new Diana(new AgentFactory()))
-    ->addJob('1', new BasicJob('1'), new PeriodicTiming(new DateInterval('PT1S')))
-    ->addJob('2', new BasicJob('2'), new PeriodicTiming(new DateInterval('PT5S')))
-    ->addJob('3', new BasicJob('3'), new PeriodicTiming(new DateInterval('PT10S')))
-    ->addJob('4', new BasicJob('4'), new PeriodicTiming(new DateInterval('PT30S')))
-    ->addJob('5', new BasicJob('5'), new PeriodicTiming(new DateInterval('PT1M')))
+    ->addJob('1', new BasicJob('1'), new PeriodicTiming(new DateInterval('PT1S'), true))
+    ->addJob('2', new BasicJob('2'), new PeriodicTiming(new DateInterval('PT5S'), true))
+    ->addJob('3', new BasicJob('3'), new PeriodicTiming(new DateInterval('PT10S'), true))
+    ->addJob('4', new BasicJob('4'), new PeriodicTiming(new DateInterval('PT30S'), true))
+    ->addJob('5', new BasicJob('5'), new PeriodicTiming(new DateInterval('PT1M'), true))
     ->on('shutdown', function (Diana $master) {
         echo "Master Shutdown.\n";
     })
