7ad49dd6989c48c51c2cff31e00e697a523c52a8
hsldymq <hsldymq@qq.com>
2019-09-19 13:52:53 +0800

    update doc comment

--- src/Timer/PeriodicTiming.php
+++ src/Timer/PeriodicTiming.php
@@ -16,6 +16,8 @@ class PeriodicTiming implements TimingInterface
     /**
      * @param \DateInterval $interval 周期间隔时长
      * @param bool $isContinuous 当为true时, 立刻开始对触发开始计时,不用等待job执行完成.
+     *                           当为false时, 会等待job完成之后在开始新一轮计时.
+     *                           false主要针对类似于while (true) { doSomething; sleep($x); }这种模式的死循环逻辑,提供等价的形式.
      */
     public function __construct(\DateInterval $interval, bool $isContinuous)
     {
