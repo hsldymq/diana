77f79dd220e191e55fb2a673163b28a9fda7697e
hsldymq <hsldymq@qq.com>
2019-09-23 12:22:56 +0800

    完善接口

--- src/Job/RepeaterInterface.php
+++ src/Job/RepeaterInterface.php
@@ -6,5 +6,19 @@ namespace Archman\Diana\Job;
 
 interface RepeaterInterface
 {
-    
+    /**
+     * 是否可以再一次重复执行.
+     *
+     * @param \DateTimeInterface $current
+     *
+     * @return bool
+     */
+    public function isRepeatable(\DateTimeInterface $current): bool;
+
+    /**
+     * 在一次execute执行完成到下一次execute开始执行之间需要等待的间隔时间.
+     *
+     * @return float 单位: 秒
+     */
+    public function getRepetitionInterval(): float;
 }
